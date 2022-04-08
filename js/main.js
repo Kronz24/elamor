9f991cbe4afd6177084e4aaa769f9627a687a0ca
Kronz24 <102811689+Kronz24@users.noreply.github.com>
2022-04-07 23:54:26 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function suma(){
+    var a;
+    var b;
+    
+    
+    a = parseInt (prompt("Por favor ingrese el primer valor"));
+    b = parseInt(prompt ("Por favor ingrese el segundo valor"));
+   
+    
+    alert("El resultado de la suma es : " + suma);
+
+}
